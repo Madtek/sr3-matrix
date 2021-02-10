9bb3485c2a737ac60647058477c0305629b01a47
madtek@gmx.net <gregor@sklorz.net>
2021-02-10 17:22:53 +0100

    basic generator, save and load

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { HostManager } from './comp/host/Host';
+import { Core } from './common/core';
+import { Hint } from './comp/Hint';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	let tmpNr: number = 0;
+
+	return (
+	<div className="App">
+		
+		<div style={{width: "50%"}}>
+		
+			<HostManager />
+		</div>
+		
+		<div style={{width: "50%"}}>
+			<Hint />
+		</div>
+	
+		<div>
+		</div>
+	</div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {Provider} from 'react-redux'
+// import store from './redux/store'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>, 
+
   document.getElementById('root')
 );
 
@@ -15,3 +20,9 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// <Provider store={store}>
+// 		<React.StrictMode>
+// 			<App />
+// 		</React.StrictMode>
+// 	</Provider>,
